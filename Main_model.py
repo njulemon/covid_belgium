54044131c30525d5b2cee8232eabd5fd575f8251
Nicolas Julémont <nicolas.julemont@gmail.com>
2020-04-08 16:01:21 +0200

    updated the model

--- Main_model.py
+++ Main_model.py
@@ -5,22 +5,26 @@ from DataAccessObject import DataAccessObject
 from Enums import Country, PatientCase, PatientCategory
 import matplotlib.pyplot as plt
 
+from SEIR_model import SEIRModel
+
 if __name__ == "__main__":
     """
     Only for debug purpose.
     """
-    dao = DataAccessObject(Country.belgium)
+    # dao = DataAccessObject(Country.belgium)
+
+    model = SEIRModel(Country.belgium)
 
     # figure
-    fig, ax = plt.subplots(figsize=(10, 7))
+    # fig, ax = plt.subplots(figsize=(10, 7))
 
-    # plotter
-    ax_plotter = AxesPlotter(dao)
-
-    # try to plot death
-    ax_plotter.plot(ax, (PatientCase.hospitalization_total, PatientCategory.country), cumsum=False, log=True)
+    # # plotter
+    # ax_plotter = AxesPlotter(dao)
+    #
+    # # try to plot death
+    # ax_plotter.plot(ax, (PatientCase.hospitalization_total, PatientCategory.country), cumsum=False, log=True)
 
     # make dates readable (rotation)
-    fig.autofmt_xdate()
-
-    plt.show()
+    # fig.autofmt_xdate()
+    #
+    # plt.show()
